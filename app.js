30c03c037f5ffb4172bf085b164146445e9d1df0
ctiller15 <christiller2015@gmail.com>
2017-07-14 02:15:46 -0400

    Fix reset text, add method to change square num, transpose all squares

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ var red;
 var green;
 var blue;
 var correctColor;
+
+// selecting the important elements
 var infoText = document.querySelector("p");
 var colorHead = document.querySelector("h2");
 var resetButton = document.querySelector("button");
+var gameArea = document.querySelector("#gameArea");
 
-// Selecting all squares.
-var squares = document.querySelectorAll(".square");
+var numInput = document.querySelector("input");
+
+// starting with a set number of squares.
+var numSquares;
+numSquares = Number(numInput.value);
+
+// creating all of the square divs.
+function createSquares(num){
+	gameArea.innerHTML = "";
+	for(var i = 0; i < numSquares; i++){
+		var sqDiv = document.createElement("div");
+		sqDiv.classList.add("square");
+		gameArea.appendChild(sqDiv);
+	}
+}
 
 function randomizeColors(sqList){
 	// Randomizing the rgb values for every single square.
@@ -42,6 +58,17 @@ function resetColors(){
 	correctColor = pickSquare(squares);
 }
 
-resetColors();
+function resetGame(){
+	createSquares(numSquares);
+	squares = document.querySelectorAll(".square");
+	resetColors();
+	infoText.textContent = "click a color";
+}
 
-resetButton.addEventListener("click", resetColors);
+resetGame();
+
+resetButton.addEventListener("click", resetGame);
+
+numInput.addEventListener("change", function(e){
+	numSquares = Number(this.value);
+});
